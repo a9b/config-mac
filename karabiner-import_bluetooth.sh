bbf10343b1c5aaa7fd18715d403ee69c4d0d9899
a9b <a9b.word@gmail.com>
2015-10-18 14:28:48 +0900

    キーボード設定エクスポート

--- karabiner-import_bluetooth.sh
+++ karabiner-import_bluetooth.sh
@@ -2,11 +2,19 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set private.remap.shift_fn_backslash.command_v 1
+/bin/echo -n .
 $cli set private.remap.control_m.enter 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f7 1
+/bin/echo -n .
+$cli set private.remap.right_alt.fn 1
 /bin/echo -n .
 $cli set private.remap.control_h.forward_delete 1
 /bin/echo -n .
 $cli set private.remap.option-=.option->=> 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f1 1
 /bin/echo -n .
 $cli set private.remap.mouse_button4_5 1
 /bin/echo -n .
@@ -14,11 +22,13 @@ $cli set private.remap.control2.option_z 1
 /bin/echo -n .
 $cli set private.remap.option_tab.command_tab 1
 /bin/echo -n .
-$cli set remap.jis_jansi 1
+$cli set remap.fn_fkeys_to_consumer_f3_lion 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f10 1
 /bin/echo -n .
 $cli set private.remap.control_k.esc 1
 /bin/echo -n .
-$cli set private.remap.shift_fn_backslash.command_v 1
+$cli set remap.fn_fkeys_to_consumer_f5 1
 /bin/echo -n .
 $cli set private.remap.forward_delete.backspace 1
 /bin/echo -n .
--- karabiner-import_hhk.sh
+++ karabiner-import_hhk.sh
@@ -2,11 +2,17 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set private.remap.shift_fn_backslash.command_v 1
+/bin/echo -n .
 $cli set private.remap.control_m.enter 1
 /bin/echo -n .
-$cli set private.remap.control_h.forward_delete 1
+$cli set private.remap.forward_delete.backspace 1
+/bin/echo -n .
+$cli set remap.fnF1_F12_to_functional 1
 /bin/echo -n .
 $cli set private.remap.option-=.option->=> 1
+/bin/echo -n .
+$cli set private.remap.control_h.forward_delete 1
 /bin/echo -n .
 $cli set private.remap.mouse_button4_5 1
 /bin/echo -n .
@@ -16,10 +22,12 @@ $cli set private.remap.option_tab.command_tab 1
 /bin/echo -n .
 $cli set remap.jis_jansi 1
 /bin/echo -n .
+$cli set general.passthrough_capslock_led_status 1
+/bin/echo -n .
+$cli set private.f19_hack 1
+/bin/echo -n .
 $cli set private.remap.control_k.esc 1
 /bin/echo -n .
-$cli set private.remap.shift_fn_backslash.command_v 1
-/bin/echo -n .
-$cli set private.remap.forward_delete.backspace 1
+$cli set private.remap.backspace.forward_delete 1
 /bin/echo -n .
 /bin/echo
